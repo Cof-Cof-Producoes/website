aee2de13b171731e909721fd29cd674680568967
Tarcísio Gaspar de Souza Filho <117198474+tarcisiogsouza@users.noreply.github.com>
2024-05-17 10:42:45 -0300

    Add files via upload

---
+++ Script/Store-Page/signpremium.js
@@ -0,0 +1,36 @@
+window.revelar = ScrollReveal({reset:true})
+
+revelar.reveal('.nav-bar',
+{
+    duration: 1000,
+    distance: '100px',
+    origin: 'top',
+    delay: 300
+
+})
+
+revelar.reveal('.box',
+{
+    duration: 1000,
+    distance: '100px',
+    origin: 'bottom',
+    delay: 500
+
+})
+
+revelar.reveal('.footer',
+{
+    duration: 1000,
+    distance: '200px',
+    origin: 'left',
+    delay: 800
+
+})
+
+revelar.reveal('.interface',
+{
+    duration: 1000,
+    origin: 'bottom',
+    delay: 100
+
+})
---
+++ Script/Store-Page/store.js
@@ -0,0 +1,49 @@
+window.revelar = ScrollReveal({reset:true})
+
+revelar.reveal('.logo-box',
+{
+    duration: 1500,
+    distance: '180px',
+    origin: 'top',
+    delay: 300,
+
+})
+
+revelar.reveal('.links',
+{
+    duration: 1500,
+    distance: '150px',
+    origin: 'bottom',
+    delay: 600,
+
+})
+
+
+
+
+revelar.reveal('.loja-montink',
+{
+    duration: 1700,
+    distance: '180px',
+    origin: 'bottom',
+    delay: 600,
+
+})
+
+revelar.reveal('.premium-img',
+{
+    duration: 1700,
+    distance: '180px',
+    origin: 'bottom',
+    delay: 800,
+
+})
+
+revelar.reveal('.bag-img',
+{
+    duration: 1700,
+    distance: '180px',
+    origin: 'bottom',
+    delay: 1000,
+
+})
