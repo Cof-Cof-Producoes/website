062add1a90ad96d30ef88f9af6046cd4003738bc
Tarcísio Gaspar de Souza Filho <tarcisiogsouza@gmail.com>
2024-05-30 17:29:32 -0300

    Media Query - Store page

--- html/Script/Store-Page/store.js
+++ html/Script/Store-Page/store.js
@@ -3,7 +3,7 @@ window.revelar = ScrollReveal({reset:true})
 revelar.reveal('.logo-box',
 {
     duration: 1500,
-    distance: '180px',
+    distance: '50px',
     origin: 'top',
     delay: 300,
 
@@ -12,7 +12,7 @@ revelar.reveal('.logo-box',
 revelar.reveal('.links',
 {
     duration: 1500,
-    distance: '150px',
+    distance: '10px',
     origin: 'bottom',
     delay: 600,
 
@@ -21,29 +21,29 @@ revelar.reveal('.links',
 
 
 
-revelar.reveal('.loja-montink',
-{
-    duration: 1700,
-    distance: '180px',
-    origin: 'bottom',
-    delay: 600,
+// revelar.reveal('.loja-montink',
+// {
+//     duration: 1700,
+//     distance: '10px',
+//     origin: 'bottom',
+//     delay: 600,
 
-})
+// })
 
-revelar.reveal('.premium-img',
-{
-    duration: 1700,
-    distance: '180px',
-    origin: 'bottom',
-    delay: 800,
+// revelar.reveal('.premium-img',
+// {
+//     duration: 1700,
+//     distance: '10px',
+//     origin: 'bottom',
+//     delay: 800,
 
-})
+// })
 
-revelar.reveal('.bag-img',
-{
-    duration: 1700,
-    distance: '180px',
-    origin: 'bottom',
-    delay: 1000,
+// revelar.reveal('.bag-img',
+// {
+//     duration: 1700,
+//     distance: '10px',
+//     origin: 'bottom',
+//     delay: 1000,
 
-})
+// })
