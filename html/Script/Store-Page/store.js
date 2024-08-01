6e8fc3f11f1f25a35dd2d2b2513b4a1e69b3cf52
Tarcísio Gaspar de Souza Filho <tarcisiogsouza@gmail.com>
2024-08-01 10:27:26 -0300

    Store update V:1.0.2
    Loja atualizada

--- html/Script/Store-Page/store.js
+++ html/Script/Store-Page/store.js
@@ -1,49 +1,29 @@
-window.revelar = ScrollReveal({reset:true})
 
-revelar.reveal('.logo-box',
-{
-    duration: 1500,
-    distance: '50px',
-    origin: 'top',
-    delay: 300,
+const menuBtn = document.querySelector("#menu_btn");
+const mobileMenuBtn = document.querySelector("#mobile_menu_btn");
 
+// Abrir menu lateral
+menuBtn.addEventListener("click", function(){
+    let menuBar = document.querySelector(".menu");
+    menuBar.classList.toggle("hidden");
+
+    let pageVestuario = document.querySelector("#vestuario");
+    pageVestuario.classList.toggle("pagemin");
+
+    let pageAcessorios = document.querySelector("#acessorios");
+    pageAcessorios.classList.toggle("pagemin");
+
+    let pagePremium = document.querySelector("#premium");
+    pagePremium.classList.toggle("pagemin");
+
+    let pageApoiar = document.querySelector("#apoiar");
+    pageApoiar.classList.toggle("pagemin");
+
+
+});
+
+// abrir menu mobile
+mobileMenuBtn.addEventListener("click", function(){
+    let menuBar = document.querySelector(".mobile_menu");
+    menuBar.classList.toggle("expand");
 })
-
-revelar.reveal('.links',
-{
-    duration: 1500,
-    distance: '10px',
-    origin: 'bottom',
-    delay: 600,
-
-})
-
-
-
-
-// revelar.reveal('.loja-montink',
-// {
-//     duration: 1700,
-//     distance: '10px',
-//     origin: 'bottom',
-//     delay: 600,
-
-// })
-
-// revelar.reveal('.premium-img',
-// {
-//     duration: 1700,
-//     distance: '10px',
-//     origin: 'bottom',
-//     delay: 800,
-
-// })
-
-// revelar.reveal('.bag-img',
-// {
-//     duration: 1700,
-//     distance: '10px',
-//     origin: 'bottom',
-//     delay: 1000,
-
-// })
